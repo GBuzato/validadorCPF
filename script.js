4a289314b412a5e8344254db0b10dfe5173e2f03
Gabriella Buzato <gabriella-buzato@hotmail.com>
2022-05-16 15:04:55 -0300

    validação segundo digito

--- script.js
+++ script.js
@@ -13,11 +13,25 @@ function validaCPF(cpf) {
         for(let index = 10; index > 1; index -= 1){
             soma += numeros.charAt(10 - index) * index;
         }
-        console.log(soma);
 
         let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
+        
+        // validação do primeiro digito
         if(resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for(let cont = 11; cont > 1; cont -= 1) {
+            soma += numeros.charAt(11 - cont) * cont;
+        }
+        
+        //validação do segundo digito
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        if(resultado != digitos.charAt(1)) {
             return false;
         }
 
@@ -27,6 +41,8 @@ function validaCPF(cpf) {
 
 function validacao() {
     console.log('Iniciando validação CPF');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
     let cpf = document.getElementById('CPF-digitado').value;
     console.log(cpf);
